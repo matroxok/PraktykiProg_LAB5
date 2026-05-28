a5c970b0447fa0874e8bb51b4e885e98f3e833d1
matroxok <mateusz.kozera@matrox.dev>
2026-05-28 15:50:23 +0200

    added: docstring documents in code

--- utils.py
+++ utils.py
@@ -1,13 +1,17 @@
 # Exemplary calculator functions
 
 def add (a: int , b: int) -> int :
+    """Add two numbers a + b"""
     return a + b
 
 def subtract (a: int , b: int) -> int :
+    """Substract two numbers a - b"""
     return a - b
 
 def multiply (a: int , b: int) -> int :
+    """Multiply two numbers a * b"""
     return a * b
 
 def divide ( a: int , b : int ) -> float :
+    """Divide two numbers a / b"""
     return a / b
