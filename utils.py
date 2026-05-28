087187927a5110726b03a2f5948abb684008ff50
matroxok <mateusz.kozera@matrox.dev>
2026-05-28 15:53:33 +0200

    fix: used pylint and black

--- utils.py
+++ utils.py
@@ -1,17 +1,22 @@
 # Exemplary calculator functions
+"""Simple python calculator"""
 
-def add (a: int , b: int) -> int :
+
+def add(a: int, b: int) -> int:
     """Add two numbers a + b"""
     return a + b
 
-def subtract (a: int , b: int) -> int :
+
+def subtract(a: int, b: int) -> int:
     """Substract two numbers a - b"""
     return a - b
 
-def multiply (a: int , b: int) -> int :
+
+def multiply(a: int, b: int) -> int:
     """Multiply two numbers a * b"""
     return a * b
 
-def divide ( a: int , b : int ) -> float :
+
+def divide(a: int, b: int) -> float:
     """Divide two numbers a / b"""
     return a / b
